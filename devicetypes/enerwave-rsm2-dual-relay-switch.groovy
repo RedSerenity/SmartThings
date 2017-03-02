9964180967d1b6276c14e61b1b46615925f27c02
Tyler Andersen <tyler.andersen@vivint.com>
2017-03-02 12:18:29 -0700

    Playing around

--- devicetypes/enerwave-rsm2-dual-relay-switch.groovy
+++ devicetypes/enerwave-rsm2-dual-relay-switch.groovy
@@ -1,74 +1,73 @@
 /**
- *  
- *	Enerwave RSM2 Dual Relay Switch Device Type
- *  
- *	Device Type supports all the feautres of the Enerwave RSM2 device including both switches, 
- *	and the AllOn/AllOff functionality. 
+ *
+ * 	Enerwave RSM2 Dual Relay Switch Device Type
+ *
+ * 	Device Type supports all the feautres of the Enerwave RSM2 device including both switches,
+ * 	and the AllOn/AllOff functionality.
  *      https://github.com/erocm123/SmartThingsPublic/blob/master/devicetypes/erocm123/enerwave-rsm2-dual-relay-switch.src/enerwave-rsm2-dual-relay-switch.groovy
  */
- 
+
 metadata {
-definition (name: "RS Enerwave RSM2 Dual Relay Switch", namespace: "RedSerenityDev", author: "Red Serenity") {
-capability "Switch"
-capability "Polling"
-capability "Configuration"
-capability "Refresh"
-capability "Zw Multichannel"
+    definition(name: "RS Enerwave RSM2 Dual Relay Switch", namespace: "RedSerenityDev", author: "Red Serenity") {
+        capability "Switch"
+        capability "Polling"
+        capability "Configuration"
+        capability "Refresh"
+        capability "Zw Multichannel"
 
-attribute "switch1", "string"
-attribute "switch2", "string"
+        attribute "switch1", "string"
+        attribute "switch2", "string"
 
-command "on1"
-command "off1"
-command "on2"
-command "off2"
+        command "on1"
+        command "off1"
+        command "on2"
+        command "off2"
 
-fingerprint deviceId: "0x1001", inClusters:"0x86, 0x72, 0x85, 0x60, 0x8E, 0x25, 0x20, 0x70, 0x27"
-
-}
-
-simulator {
-status "on": "command: 2003, payload: FF"
-status "off": "command: 2003, payload: 00"
-
-// reply messages
-reply "2001FF,delay 100,2502": "command: 2503, payload: FF"
-reply "200100,delay 100,2502": "command: 2503, payload: 00"
-}
-
-tiles(scale: 2){
-
-    multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-			}
-	}
-	standardTile("switch1", "device.switch1",canChangeIcon: true, width: 2, height: 2) {
-		state "on", label: "switch1", action: "off1", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-		state "off", label: "switch1", action: "on1", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-    }
-	standardTile("switch2", "device.switch2",canChangeIcon: true, width: 2, height: 2) {
-		state "on", label: "switch2", action: "off2", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-		state "off", label: "switch2", action: "on2", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-    }
-    standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-		state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+        fingerprint deviceId: "0x1001", inClusters: "0x86, 0x72, 0x85, 0x60, 0x8E, 0x25, 0x20, 0x70, 0x27"
     }
 
-    standardTile("configure", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-		state "default", label:"", action:"configure", icon:"st.secondary.configure"
+    simulator {
+        status "on": "command: 2003, payload: FF"
+        status "off": "command: 2003, payload: 00"
+
+        // reply messages
+        reply "2001FF,delay 100,2502": "command: 2503, payload: FF"
+        reply "200100,delay 100,2502": "command: 2503, payload: 00"
     }
 
-    main(["switch","switch1", "switch2"])
-    details(["switch","switch1","switch2","refresh","configure"])
-}
+    tiles(scale: 2) {
+
+        multiAttributeTile(name: "switch", type: "lighting", width: 6, height: 4, canChangeIcon: true) {
+            tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+                attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+            }
+        }
+        standardTile("switch1", "device.switch1", canChangeIcon: true, width: 2, height: 2) {
+            state "on", label: "switch1", action: "off1", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+            state "off", label: "switch1", action: "on1", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+        }
+        standardTile("switch2", "device.switch2", canChangeIcon: true, width: 2, height: 2) {
+            state "on", label: "switch2", action: "off2", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+            state "off", label: "switch2", action: "on2", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+        }
+        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label: "", action: "refresh.refresh", icon: "st.secondary.refresh"
+        }
+
+        standardTile("configure", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label: "", action: "configure", icon: "st.secondary.configure"
+        }
+
+        main(["switch", "switch1", "switch2"])
+        details(["switch", "switch1", "switch2", "refresh", "configure"])
+    }
     /* Waiting to hear back from manufacturer regarding what preferences can be set for this device. 
 	preferences {
         input "switchType", "enum", title: "Switch Type", defaultValue: "1", displayDuringSetup: true, required: false, options: [
                 "1":"Toggle",
                 "2":"Momentary"]
-  }*/
+    }*/
 }
 
 def parse(String description) {
@@ -83,25 +82,23 @@ def parse(String description) {
     return result
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
-{
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
     // Not used
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
-	sendEvent(name: "switch", value: cmd.value ? "on" : "off", type: "digital")
+    sendEvent(name: "switch", value: cmd.value ? "on" : "off", type: "digital")
     def result = []
-    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
-    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     response(delayBetween(result, 1000)) // returns the result of reponse()
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd)
-{
+def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
     sendEvent(name: "switch", value: cmd.value ? "on" : "off", type: "digital")
     def result = []
-    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
-    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     response(delayBetween(result, 1000)) // returns the result of reponse()
 }
 
@@ -117,60 +114,58 @@ def zwaveEvent(physicalgraph.zwave.commands.meterv3.MeterReport cmd) {
     return result
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCapabilityReport cmd) 
-{
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCapabilityReport cmd) {
     log.debug "multichannelv3.MultiChannelCapabilityReport $cmd"
-    if (cmd.endPoint == 2 ) {
+    if (cmd.endPoint == 2) {
         def currstate = device.currentState("switch2").getValue()
         if (currstate == "on")
-        	sendEvent(name: "switch2", value: "off", isStateChange: true, display: false)
+            sendEvent(name: "switch2", value: "off", isStateChange: true, display: false)
         else if (currstate == "off")
-        	sendEvent(name: "switch2", value: "on", isStateChange: true, display: false)
-    }
-    else if (cmd.endPoint == 1 ) {
+            sendEvent(name: "switch2", value: "on", isStateChange: true, display: false)
+    } else if (cmd.endPoint == 1) {
         def currstate = device.currentState("switch1").getValue()
         if (currstate == "on")
-        sendEvent(name: "switch1", value: "off", isStateChange: true, display: false)
+            sendEvent(name: "switch1", value: "off", isStateChange: true, display: false)
         else if (currstate == "off")
-        sendEvent(name: "switch1", value: "on", isStateChange: true, display: false)
+            sendEvent(name: "switch1", value: "on", isStateChange: true, display: false)
     }
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
-   def map = [ name: "switch$cmd.sourceEndPoint" ]
-    
-   switch(cmd.commandClass) {
-      case 32:
-         if (cmd.parameter == [0]) {
-            map.value = "off"
-         }
-         if (cmd.parameter == [255]) {
-            map.value = "on"
-         }
-         createEvent(map)
-         break
-      case 37:
-         if (cmd.parameter == [0]) {
-            map.value = "off"
-         }
-         if (cmd.parameter == [255]) {
-            map.value = "on"
-         }
-         break
+    def map = [name: "switch$cmd.sourceEndPoint"]
+
+    switch (cmd.commandClass) {
+        case 32:
+            if (cmd.parameter == [0]) {
+                map.value = "off"
+            }
+            if (cmd.parameter == [255]) {
+                map.value = "on"
+            }
+            createEvent(map)
+            break
+        case 37:
+            if (cmd.parameter == [0]) {
+                map.value = "off"
+            }
+            if (cmd.parameter == [255]) {
+                map.value = "on"
+            }
+            break
     }
     def events = [createEvent(map)]
     if (map.value == "on") {
-            events += [createEvent([name: "switch", value: "on"])]
+        events += [createEvent([name: "switch", value: "on"])]
     } else {
-         def allOff = true
-         (1..2).each { n ->
-             if (n != cmd.sourceEndPoint) {
-                 if (device.currentState("switch${n}").value != "off") allOff = false
-             }
-         }
-         if (allOff) {
-             events += [createEvent([name: "switch", value: "off"])]
-         }
+        def allOff = true
+        (1..2).each { n ->
+            if (n != cmd.sourceEndPoint) {
+                if (device.currentState("switch${n}").value != "off") allOff = false
+            }
+        }
+        if (allOff) {
+            events += [createEvent([name: "switch", value: "off"])]
+        }
     }
     events
 }
@@ -182,103 +177,104 @@ def zwaveEvent(physicalgraph.zwave.Command cmd) {
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.switchallv1.SwitchAllReport cmd) {
-   log.debug "SwitchAllReport $cmd"
+    log.debug "SwitchAllReport $cmd"
 }
 
 def refresh() {
-	def cmds = []
+    def cmds = []
     cmds << zwave.manufacturerSpecificV2.manufacturerSpecificGet().format()
-	cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
-    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
-	delayBetween(cmds, 1000)
+    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
+    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
+    delayBetween(cmds, 1000)
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
-	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
-	log.debug "msr: $msr"
+    def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
+    log.debug "msr: $msr"
     updateDataValue("MSR", msr)
 }
 
 def poll() {
-	def cmds = []
-	cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
-    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
-	delayBetween(cmds, 1000)
+    def cmds = []
+    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
+    cmds << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
+    delayBetween(cmds, 1000)
 }
 
 def configure() {
-	log.debug "configure() called"
+    log.debug "configure() called"
     def cmds = []
     cmds << zwave.configurationV1.configurationSet(parameterNumber: 3, configurationValue: [1]).format()
     cmds << zwave.configurationV1.configurationGet(parameterNumber: 3).format()
     if (switchType != null && switchType.value != null) {
-    switch (switchType.value as String) {
-       case "1":
-       log.debug "Configuring device as a Toggle switch"
-       //cmds << zwave.configurationV1.configurationSet(parameterNumber: 3, configurationValue: [3]).format()	
-       //cmds << zwave.configurationV1.configurationGet(parameterNumber: 3).format()
-       break
-       case "2":
-       log.debug "Configuring device as a Momentary switch"
-       //cmds << zwave.configurationV1.configurationSet(parameterNumber: 3, configurationValue: [1]).format()	
-       //cmds << zwave.configurationV1.configurationGet(parameterNumber: 3).format()
-       break
-       default:
-       log.debug "No valid device type chosen"
-       break
+        switch (switchType.value as String) {
+            case "1":
+                log.debug "Configuring device as a Toggle switch"
+                //cmds << zwave.configurationV1.configurationSet(parameterNumber: 3, configurationValue: [3]).format()
+                //cmds << zwave.configurationV1.configurationGet(parameterNumber: 3).format()
+                break
+            case "2":
+                log.debug "Configuring device as a Momentary switch"
+                //cmds << zwave.configurationV1.configurationSet(parameterNumber: 3, configurationValue: [1]).format()
+                //cmds << zwave.configurationV1.configurationGet(parameterNumber: 3).format()
+                break
+            default:
+                log.debug "No valid device type chosen"
+                break
+        }
     }
-    }
-    
-    if ( cmds != [] && cmds != null ) return delayBetween(cmds, 2000) else return
+
+    if (cmds != [] && cmds != null) return delayBetween(cmds, 2000) else return
 }
 /**
-* Triggered when Done button is pushed on Preference Pane
-*/
-def updated()
-{
-	log.debug "Preferences have been changed. Attempting configure()"
+ * Triggered when Done button is pushed on Preference Pane
+ */
+def updated() {
+    log.debug "Preferences have been changed. Attempting configure()"
     def cmds = configure()
     response(cmds)
 }
-def on() { 
-   delayBetween([
-        zwave.switchAllV1.switchAllOn().format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
+
+def on() {
+    delayBetween([
+            zwave.switchAllV1.switchAllOn().format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     ], 1000)
 }
+
 def off() {
-   delayBetween([
-        zwave.switchAllV1.switchAllOff().format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:2, commandClass:37, command:2).format()
+    delayBetween([
+            zwave.switchAllV1.switchAllOff().format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     ], 1000)
 }
 
 def on1() {
     delayBetween([
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:1, parameter:[255]).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 1, parameter: [255]).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
     ], 1000)
 }
 
 def off1() {
     delayBetween([
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:1, parameter:[0]).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1, destinationEndPoint:1, commandClass:37, command:2).format()
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 1, parameter: [0]).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 1, destinationEndPoint: 1, commandClass: 37, command: 2).format()
     ], 1000)
 }
 
 def on2() {
     delayBetween([
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:2, destinationEndPoint:2, commandClass:37, command:1, parameter:[255]).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:2, destinationEndPoint:2, commandClass:37, command:2).format()
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 2, destinationEndPoint: 2, commandClass: 37, command: 1, parameter: [255]).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 2, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     ], 1000)
 }
 
 def off2() {
     delayBetween([
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:2, destinationEndPoint:2, commandClass:37, command:1, parameter:[0]).format(),
-        zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:2, destinationEndPoint:2, commandClass:37, command:2).format()
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 2, destinationEndPoint: 2, commandClass: 37, command: 1, parameter: [0]).format(),
+            zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint: 2, destinationEndPoint: 2, commandClass: 37, command: 2).format()
     ], 1000)
 }
